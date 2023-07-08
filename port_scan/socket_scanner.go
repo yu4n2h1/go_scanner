85c80b765e89b2dfd2ecb6356af5d7ff0a19e420
l0tk3 <1037730299@qq.com>
2023-07-08 20:04:29 +0800

    optimize socket scan

--- port_scan/socket_scanner.go
+++ port_scan/socket_scanner.go
@@ -12,13 +12,12 @@ func socket_conn(port int, ip string, wg *sync.WaitGroup) bool {
 	wg.Add(1)
 	d := &net.Dialer{Timeout: time.Duration(3) * time.Second}
 	target := ip + ":" + strconv.Itoa(port)
-	conn, err := d.Dial("tcp", target)
+	conn, err := d.Dial("tcp4", target)
 
 	if err != nil {
 		return false
 	} else {
 		defer conn.Close()
-		// fmt.Println(port, "is alive")
 		return true
 	}
 
@@ -28,46 +27,36 @@ func Socket_scan(ip string) {
 	var alive_prot []int
 	var wg sync.WaitGroup
 	var sub_wg sync.WaitGroup
-	lock := &sync.Mutex{}
 	port_chan := make(chan int, 65536)
 	alive_chan := make(chan int, 65536)
-	thread_num := 10000
-	is_close := false
+	thread_num := 800
 	for i := 0; i < thread_num; i++ {
 		go func() {
-			wg.Add(1)
-			defer wg.Done()
 			for port := range port_chan {
 				if socket_conn(port, ip, &sub_wg) {
+					fmt.Println(ip + ":" + strconv.Itoa(port) + " is open")
 					alive_chan <- port
 				}
-				sub_wg.Done()
+				wg.Done()
 			}
-			sub_wg.Wait()
-
-			lock.Lock()
-			if is_close == false {
-				close(alive_chan)
-				is_close = true
-			}
-			lock.Unlock()
 		}()
 	}
 
 	go func() {
-		wg.Add(1)
-		defer wg.Done()
 		for alive := range alive_chan {
 			alive_prot = append(alive_prot, alive)
+			sub_wg.Done()
 		}
 	}()
 
 	for i := 0; i <= 65535; i++ {
+		wg.Add(1)
 		port_chan <- i
 	}
 
 	close(port_chan)
 
 	wg.Wait()
+	close(alive_chan)
 	fmt.Println(ip, "->", alive_prot)
 }
