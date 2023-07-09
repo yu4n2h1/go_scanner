2340f4aeef900905c2ca0d225486628f2fad6607
yuanzhimengbian <10920845+yuanzhimengbian@user.noreply.gitee.com>
2023-07-09 21:08:58 +0800

    use CIDR

--- main.go
+++ main.go
@@ -3,21 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go_scanner/global"
 	"go_scanner/icmp_scan"
 	"go_scanner/ping_scan"
 	"go_scanner/port_scan"
 	"go_scanner/tools"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	raddr := flag.String("i", "127.0.0.1", "Input the ip address you want to scan")
-	mask := flag.Int("m", 31, "Input the mask of target network")
+	global.CIDR = flag.String("h", "127.0.0.1/32", "Scan IP addresses based on input in CIDR notation.")
+
+	// raddr := flag.String("i", "127.0.0.1", "Input the ip address you want to scan")
+	// mask := flag.Int("m", 31, "Input the mask of target network")
 	pingis := flag.Bool("ping", false, "Input the ping ro not")
 	flag.Parse()
+	args := strings.Split(*global.CIDR, "/")
+	raddr := args[0]
+	mask, err := strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// laddr := tools.Get_self(*raddr)
 	var isping bool = *pingis
-	min, max := tools.Get_ip_range(int(tools.Ip2int(*raddr)), *mask)
+	min, max := tools.Get_ip_range(int(tools.Ip2int(raddr)), mask)
 	ipslist := make([]string, max-min)
 	for i := min; i <= max; i++ {
 		ipslist = append(ipslist, tools.Int2ip(int32(i)))
