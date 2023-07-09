949cad6da634d6fb7db037c47304cabe4dc751fe
yuanzhimengbian <10920845+yuanzhimengbian@user.noreply.gitee.com>
2023-07-09 10:39:28 +0800

    ping_scan初步完成

--- main.go
+++ main.go
@@ -4,22 +4,31 @@ import (
 	"flag"
 	"fmt"
 	"go_scanner/icmp_scan"
+	"go_scanner/ping_scan"
 	"go_scanner/port_scan"
 )
 
 func main() {
 	raddr := flag.String("i", "127.0.0.1", "Input the ip address you want to scan")
 	mask := flag.Int("m", 31, "Input the mask of target network")
+	pingis := flag.Bool("ping", false, "Input the ping ro not")
 	flag.Parse()
 
 	// laddr := tools.Get_self(*raddr)
+	if *pingis {
+		fmt.Println("使用ping")
+		res := ping_scan.CmdPing(*raddr, *mask)
+		fmt.Println(res)
+		fmt.Println(len(res))
+	} else {
+		res := icmp_scan.Ping(*raddr, *mask)
+		fmt.Println(res)
+		fmt.Println(len(res))
 
-	res := icmp_scan.Ping(*raddr, *mask)
-	fmt.Println(res)
-	fmt.Println(len(res))
+		for _, ip := range res {
+			port_scan.Socket_scan(ip)
+		}
 
-	for _, ip := range res {
-		port_scan.Socket_scan(ip)
 	}
 
 	// ip := "101.43.140.240"
