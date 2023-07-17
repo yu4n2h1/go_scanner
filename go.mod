4daf2570776b55c5aad2ffed274e47877b8fe205
smcq-dbc <2239175123@qq.com>
2023-07-17 21:39:14 +0900

    更新指纹库及匹配方法

--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 )
 
-require golang.org/x/sys v0.10.0 // indirect
+require (
+	github.com/dlclark/regexp2 v1.10.0
+	golang.org/x/sys v0.10.0 // indirect
+)
--- info_scan/info_scan.go
+++ info_scan/info_scan.go
@@ -3,20 +3,110 @@ package info_scan
 import (
 	"encoding/json"
 	"fmt"
-	"go_scanner/honeypot_ident"
 	"io/ioutil"
 	"net"
 	"regexp"
-	"strings"
 	"time"
+
+	"github.com/dlclark/regexp2"
 )
 
-type Service struct {
-	Name    string `json:"name"`
-	Feature string `json:"feature"`
+type VersionInfo struct {
+	CPEName           string `json:"cpename"`
+	DeviceType        string `json:"devicetype"`
+	Hostname          string `json:"hostname"`
+	Info              string `json:"info"`
+	OperatingSystem   string `json:"operatingsystem"`
+	VendorProductName string `json:"vendorproductname"`
+	Version           string `json:"version"`
 }
 
-func getPortInfo(ip string, port int) (string, error) {
+type Match struct {
+	Pattern     string      `json:"pattern"`
+	Name        string      `json:"name"`
+	PatternFlag string      `json:"pattern_flag"`
+	VersionInfo VersionInfo `json:"versioninfo"`
+}
+
+type JSONData struct {
+	Protocol     string        `json:"protocol"`
+	Probename    string        `json:"probename"`
+	Probestring  string        `json:"probestring"`
+	Ports        []interface{} `json:"ports"`
+	SSLPorts     []interface{} `json:"sslports"`
+	TotalWaitMs  string        `json:"totalwaitms"`
+	TCPWrappedMs string        `json:"tcpwrappedms"`
+	Rarity       string        `json:"rarity"`
+	Fallback     string        `json:"fallback"`
+	Matches      []Match       `json:"matches"`
+}
+
+func InfoScan(ip string, ports []int) {
+	for _, port := range ports {
+		fmt.Println("Port:", port)
+		startJudge(ip, port)
+	}
+}
+
+func startJudge(ip string, port int) {
+	// 读取 JSON 数据文件
+	jsonData, err := ioutil.ReadFile("info_scan/finger.json")
+	if err != nil {
+		fmt.Println("无法读取 JSON 数据文件:", err)
+		return
+	}
+
+	var dataArray []JSONData
+	err = json.Unmarshal(jsonData, &dataArray)
+	if err != nil {
+		fmt.Println("JSON 数据解析失败:", err)
+		return
+	}
+
+	// 遍历 JSON 数据
+	for _, data := range dataArray {
+		probename := data.Probename
+		matches := data.Matches
+
+		// 向指定的端口发送 probename 数据
+		response, err := sendProbeData(ip, port, probename)
+		// fmt.Println(response)
+		if err != nil {
+			fmt.Println("无法连接到端口:", err)
+			fmt.Println("----------------")
+			continue
+		}
+
+		// 判断返回数据是否与匹配模式相匹配
+		for _, match := range matches {
+			pattern := match.Pattern
+			name := match.Name
+			operatingSystem := match.VersionInfo.OperatingSystem
+			// vendorProductName := match.VersionInfo.VendorProductName
+			// re, err := regexp.Compile(pattern)
+			re := regexp2.MustCompile(pattern, 0)
+			matches, _ := re.FindStringMatch(response)
+			// matches := re.FindStringSubmatch(response)
+			if matches != nil {
+				versionInfo := match.VersionInfo
+				version := getVersionValue(versionInfo.Version, matches.Groups())
+				vendorProductName := getVersionValue(versionInfo.VendorProductName, matches.Groups())
+				fmt.Println("匹配成功:")
+				fmt.Println("Name:", name)
+				fmt.Println("Operating System:", operatingSystem)
+				fmt.Println("Vendor Product Name:", vendorProductName)
+				fmt.Println("Version:", version)
+				fmt.Println("----------------")
+				// 中断外部和内部循环
+				goto endLoop
+			}
+		}
+	}
+
+endLoop:
+}
+
+func sendProbeData(ip string, port int, probename string) (string, error) {
 	// 构建目标地址
 	target := fmt.Sprintf("%s:%d", ip, port)
 
@@ -31,7 +121,7 @@ func getPortInfo(ip string, port int) (string, error) {
 	conn.SetDeadline(time.Now().Add(3 * time.Second))
 
 	// 发送数据
-	_, err = conn.Write([]byte("7f519f7fa3eec7c5e3049a681c62f9f9\n"))
+	_, err = conn.Write([]byte(probename + "\n"))
 	if err != nil {
 		return "", fmt.Errorf("发送数据失败: %w", err)
 	}
@@ -47,46 +137,14 @@ func getPortInfo(ip string, port int) (string, error) {
 	return response, nil
 }
 
-func ServiceJudge(ip string, port int) (string, error) {
-	response, err := getPortInfo(ip, port)
-	if err != nil {
-		return "", fmt.Errorf("发生错误: %w", err)
+func getVersionValue(version string, matches []regexp2.Group) string {
+	for i := 1; i < len(matches); i++ {
+		placeholder := fmt.Sprintf("$%d", i)
+		version = replaceAll(version, placeholder, matches[i].Captures[0].String())
 	}
-
-	services, err := GetFeatureFromFile("info_scan/service.json")
-	if err != nil {
-		return "", fmt.Errorf("读取文件失败: %w", err)
-	}
-
-	for _, service := range services {
-		matched, err := regexp.MatchString("(?i)"+service.Feature, response)
-		if err != nil {
-			return "", fmt.Errorf("发生错误: %w", err)
-		}
-		if matched {
-			if strings.Contains(service.Name, "ssh") {
-				// honeypot_ident.DetectKippo()
-				if honeypot_ident.DetectKippo(ip, port) {
-					fmt.Println(service.Name, "may is Kippo")
-				}
-			}
-			return service.Name, nil
-		}
-	}
-	return "规则未命中", nil
+	return version
 }
 
-func GetFeatureFromFile(filename string) ([]Service, error) {
-	jsonData, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("读取 JSON 文件失败: %w", err)
-	}
-
-	var services []Service
-	err = json.Unmarshal(jsonData, &services)
-	if err != nil {
-		return nil, fmt.Errorf("解析 JSON 数据失败: %w", err)
-	}
-
-	return services, nil
+func replaceAll(s, old, new string) string {
+	return regexp.MustCompile(regexp.QuoteMeta(old)).ReplaceAllLiteralString(s, new)
 }
